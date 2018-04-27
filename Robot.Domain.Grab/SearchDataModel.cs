d17e8a8a0580a3565ff0dce7950bcffaaf160be8
GiantLiu <liuju150@outlook.com>
2018-04-27 15:37:54 +0800

    interface

---
+++ Robot.Domain.Grab/SearchDataModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.Domain.Grab
+{
+    public class SearchDataModel
+    {
+        public int ID { get; set; }
+        /// <summary>
+        /// 任务组
+        /// </summary>
+        public Guid TaskGroup { get; set; }
+        /// <summary>
+        /// 任务ID
+        /// </summary>
+        public int TaskID { get; set; }
+        public int SoruceID { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 平台地址
+        /// </summary>
+        public string SourceUrl { get; set; }
+        /// <summary>
+        /// 真实地址
+        /// </summary>
+        public string RealUrl { get; set; }
+        /// <summary>
+        /// 查询内容
+        /// </summary>
+        public string Content { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int Status { get; set; }
+    }
+}
---
+++ Robot.IService.Grab/ISearchService.cs
@@ -0,0 +1,15 @@
+﻿using Robot.Domain.Grab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.IService.Grab
+{
+    public interface ISearchService
+    {
+        Task<string> GetHtmlAsync(string keyword, int page);
+        Task<List<SearchDataModel>> GetDataAsync(string html);
+    }
+}
